1240f2347e3c60746168daeea71ce15de5bdb79b
Chanotai Mukdakul <maxzabunhotel@gmail.com>
2025-06-21 11:26:40 +0700

    :)

--- main.py
+++ main.py
@@ -170,4 +170,6 @@ if __name__ == '__main__':
         with open(USERS_FILE, 'w', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(['username', 'password', 'role'])
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
